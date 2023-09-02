61b0823b31dcc00a36abe0f4b7cc5eb0cb7c5e1c
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-02 14:51:08 +0100

    Add basic counter example for hydration testing

---
+++ packages/example/src/counter.client.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+
+export const title = 'Counter';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
+};
+
+export default Counter;
