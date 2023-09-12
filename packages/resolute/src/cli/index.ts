d4f9b6eec106ed97c16edec42f70470f07e48328
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-12 13:56:01 +0100

    Update CLI commands/arguments

--- packages/resolute/src/cli/index.ts
+++ packages/resolute/src/cli/index.ts
@@ -1,10 +1,17 @@
 #! /usr/bin/env node
 
-import { collect, Command, Help, Program, RequireAny, Required } from 'jargs';
+import { collect, Command, Help, KWArg, Program, RequireAny } from 'jargs';
 
 import { DESCRIPTION, PROGRAM } from '../constants.js';
-import buildStatic from './build/static.js';
-import serveStatic from './serve/static.js';
+import buildStatic from './build/ssg.js';
+import devStatic from './dev/ssg.js';
+
+const RENDERER = KWArg('renderer', {
+  alias: 'r',
+  description:
+    'The renderer used for the output files. ssg will output plain HTML files, and ssr will output a Node.js server.',
+  options: ['ssg', 'ssr'],
+});
 
 collect(
   Help(
@@ -18,7 +25,11 @@ collect(
       {
         description: DESCRIPTION,
         usage: `${PROGRAM} <command> [options]`,
-        examples: [`${PROGRAM} --help`, `${PROGRAM} static`],
+        examples: [
+          `${PROGRAM} --help`,
+          `${PROGRAM} build`,
+          `${PROGRAM} build --renderer ssg`,
+        ],
       },
       RequireAny(
         Command(
@@ -26,32 +37,70 @@ collect(
           {
             description: 'Build site for deployment',
             usage: `${PROGRAM} build [options]`,
-            examples: [`${PROGRAM} build static`],
+            examples: [`${PROGRAM} build --renderer ssg`],
+            callback: (tree) => {
+              if (
+                typeof tree.kwargs.renderer === 'undefined' ||
+                tree.kwargs.renderer === 'ssg'
+              ) {
+                return buildStatic();
+              }
+
+              if (tree.kwargs.renderer === 'ssr') {
+                // eslint-disable-next-line no-console
+                console.error('Server-side rendering is not yet supported');
+
+                return process.exit(1);
+              }
+
+              // eslint-disable-next-line no-console
+              console.error(
+                `Unknown renderer: ${
+                  tree.kwargs.renderer
+                }. Renderer must be one of: ${RENDERER.options.options?.join(
+                  ', '
+                )}`
+              );
+
+              return process.exit(1);
+            },
           },
-          Required(
-            Command('static', {
-              description: 'Build a static site',
-              usage: `${PROGRAM} static [options]`,
-              examples: [`${PROGRAM} static`],
-              callback: buildStatic,
-            })
-          )
+          RENDERER
         ),
         Command(
-          'serve',
+          'dev',
           {
             description: 'Serve site for development',
             usage: `${PROGRAM} serve [options]`,
-            examples: [`${PROGRAM} serve static`],
+            examples: [`${PROGRAM} serve --renderer ssg`],
+            callback: (tree) => {
+              if (
+                typeof tree.kwargs.renderer === 'undefined' ||
+                tree.kwargs.renderer === 'ssg'
+              ) {
+                return devStatic();
+              }
+
+              if (tree.kwargs.renderer === 'ssr') {
+                // eslint-disable-next-line no-console
+                console.error('Server-side rendering is not yet supported');
+
+                return process.exit(1);
+              }
+
+              // eslint-disable-next-line no-console
+              console.error(
+                `Unknown renderer: ${
+                  tree.kwargs.renderer
+                }. Renderer must be one of: ${RENDERER.options.options?.join(
+                  ', '
+                )}`
+              );
+
+              return process.exit(1);
+            },
           },
-          Required(
-            Command('static', {
-              description: 'Serve a static site',
-              usage: `${PROGRAM} static [options]`,
-              examples: [`${PROGRAM} static`],
-              callback: serveStatic,
-            })
-          )
+          RENDERER
         )
       )
     )
