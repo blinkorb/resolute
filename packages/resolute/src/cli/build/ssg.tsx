0a43ab6478c4202b8279889b2cbb5914b4b6e9ca
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-12 15:28:44 +0100

    Copy public and resolute files into out dir node modules

--- packages/resolute/src/cli/build/ssg.tsx
+++ packages/resolute/src/cli/build/ssg.tsx
@@ -1,3 +1,22 @@
-const buildStatic = async () => {};
+import path from 'node:path';
+
+import cpy from 'cpy';
+import { rimrafSync } from 'rimraf';
+
+import {
+  OUT_PATHNAME,
+  PUBLIC_FILES_GLOB,
+  RESOLUTE_PATHNAME,
+} from '../constants.js';
+
+const buildStatic = async () => {
+  rimrafSync(OUT_PATHNAME);
+  await cpy(PUBLIC_FILES_GLOB, OUT_PATHNAME);
+  await cpy(
+    path.resolve(RESOLUTE_PATHNAME, '**/*.js'),
+    path.resolve(OUT_PATHNAME, 'node-modules', '@blinkorb', 'resolute'),
+    { filter: (file) => !file.path.includes('/cli/') }
+  );
+};
 
 export default buildStatic;
