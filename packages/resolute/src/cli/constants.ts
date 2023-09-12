5dbb312d162dd5e4aec006b54933906fbfac3bff
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-12 17:44:08 +0100

    Include api in check for server modules

--- packages/resolute/src/cli/constants.ts
+++ packages/resolute/src/cli/constants.ts
@@ -17,4 +17,4 @@ export const MATCHES_LOCAL = /^[./]/;
 export const MATCHES_NODE_MODULE = /.*\/node_modules\//;
 export const MATCHES_RESOLUTE = /@blinkorb\/resolute|resolute\/build/;
 export const MATCHES_CLIENT = /\.(page|client|layout)\./;
-export const MATCHES_SERVER = /\.(server|static)\./;
+export const MATCHES_SERVER = /\.(server|static|api)\./;
