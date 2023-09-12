dbd8854a82b89859d52d1c2cad0974273bee5ff3
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-12 17:42:44 +0100

    Update how initial hydration data is loaded

--- packages/resolute/src/client.tsx
+++ packages/resolute/src/client.tsx
@@ -3,24 +3,33 @@ import { hydrateRoot } from 'react-dom/client';
 import { getModuleElement } from './utils/component.js';
 import { getModule } from './utils/module.js';
 
-const resoluteClientJson: unknown = JSON.parse(
-  document.getElementById('resolute-client-json')?.innerText ?? 'null'
-);
+const resoluteClientJson = await fetch(
+  `${window.location.protocol}//${window.location.host}${window.location.pathname}/resolute.json`
+).then((response) => {
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error(
+    `Failed to fetch resolute.json for route ${window.location.pathname}`
+  );
+});
 
 if (!resoluteClientJson || typeof resoluteClientJson !== 'object') {
-  throw new Error('Invalid resolute-client-json');
+  throw new Error('Invalid resolute.json');
 }
 
-if (!('client' in resoluteClientJson)) {
-  throw new Error('No client specified in resolute-client-json');
+if ('client' in resoluteClientJson) {
+  if (typeof resoluteClientJson.client !== 'string') {
+    throw new Error('Client must be a string in resolute.json');
+  }
+
+  const { client } = resoluteClientJson;
+
+  const clientModule = await getModule(client);
+
+  hydrateRoot(
+    window.document.body,
+    await getModuleElement(clientModule, client)
+  );
 }
-
-if (typeof resoluteClientJson.client !== 'string') {
-  throw new Error('Client must be a string in resolute-client-json');
-}
-
-const { client } = resoluteClientJson;
-
-const clientModule = await getModule(client);
-
-hydrateRoot(window.document.body, await getModuleElement(clientModule, client));
