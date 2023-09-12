4bc9db3df5ede993858640d8f903bf2b7f2e06ff
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-12 16:41:00 +0100

    Extract about component

--- packages/example/src/about.client.tsx
+++ packages/example/src/about.client.tsx
@@ -1,6 +1,8 @@
 import { createAPI } from '@blinkorb/resolute';
 import React from 'react';
 
+import AboutInfo from './components/about-info.js';
+
 const fetch =
   createAPI<typeof import('./about.server.js')>('./about.server.js');
 
@@ -9,12 +11,7 @@ export const title = 'About Us';
 const About = async () => {
   const data = await fetch('getAboutData');
 
-  return (
-    <>
-      <h1>About</h1>
-      <p>{data.content}</p>
-    </>
-  );
+  return <AboutInfo title="About" content={data.content} />;
 };
 
 export default About;
