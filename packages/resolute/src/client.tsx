17fec07d5ffd65bcfba15ad2a7e0ccfcc90c8d24
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-14 03:30:06 +0100

    Include layouts in client hydration

--- packages/resolute/src/client.tsx
+++ packages/resolute/src/client.tsx
@@ -1,5 +1,7 @@
+import React, { ReactElement } from 'react';
 import { hydrateRoot } from 'react-dom/client';
 
+import Page from './page.js';
 import { ResoluteJSON } from './types.js';
 import { getModuleElement, getProps } from './utils/component.js';
 import { getModule } from './utils/module.js';
@@ -28,5 +30,27 @@ if ('client' in resoluteClientJson) {
     allProps
   );
 
-  hydrateRoot(window.document.body, element);
+  const withLayouts = await client.layouts.reduce<Promise<ReactElement>>(
+    async (accPromise, layout) => {
+      const acc = await accPromise;
+      const layoutModule = await getModule(layout.pathname);
+      const layoutProps = layout.props ?? {};
+      const layoutElement = await getModuleElement(
+        layoutModule,
+        layout.pathname,
+        layoutProps,
+        acc
+      );
+
+      return layoutElement;
+    },
+    Promise.resolve(element)
+  );
+
+  hydrateRoot(
+    window.document.body,
+    <Page pageModule={clientModule} pathname={client.pathname}>
+      {withLayouts}
+    </Page>
+  );
 }
