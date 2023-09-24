b9cd31b8978795f6ae5ed9008a729d3fa178b1e8
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-25 00:10:57 +0100

    Setup react-jss in example

--- packages/example/src/index.layout.tsx
+++ packages/example/src/index.layout.tsx
@@ -1,39 +1,72 @@
 import { Link } from '@blinkorb/resolute';
 import React, { ReactNode } from 'react';
+import { createUseStyles, DefaultTheme, ThemeProvider } from 'react-jss';
+
+const THEME = {
+  red: '#f00',
+} satisfies DefaultTheme;
+
+const useStyles = createUseStyles((theme) => ({
+  link: {
+    color: theme.red,
+  },
+}));
+
+const Navigation = () => {
+  const styles = useStyles();
+
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link className={styles.link} href="/">
+            Home (static)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/events">
+            Events (hydrated async)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/about">
+            About (overridden layout)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/counter">
+            Counter (avoid hydration)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/client-only">
+            Client Only Content (hydrated)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/nested-layout">
+            Nested Layout (no title)
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} href="/some-path">
+            Hidden Route (helmet title)
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
 const Layout = ({
   children,
 }: {
   children: ReactNode | readonly ReactNode[];
 }) => (
-  <>
-    <header>
-      <ul>
-        <li>
-          <Link href="/">Home (static)</Link>
-        </li>
-        <li>
-          <Link href="/events">Events (hydrated async)</Link>
-        </li>
-        <li>
-          <Link href="/about">About (overridden layout)</Link>
-        </li>
-        <li>
-          <Link href="/counter">Counter (avoid hydration)</Link>
-        </li>
-        <li>
-          <Link href="/client-only">Client Only Content (hydrated)</Link>
-        </li>
-        <li>
-          <Link href="/nested-layout">Nested Layout (no title)</Link>
-        </li>
-        <li>
-          <Link href="/some-path">Hidden Route (helmet title)</Link>
-        </li>
-      </ul>
-    </header>
+  <ThemeProvider theme={THEME}>
+    <Navigation />
     {children}
-  </>
+  </ThemeProvider>
 );
 
 export default Layout;
---
+++ packages/example/src/jss.d.ts
@@ -0,0 +1,9 @@
+declare global {
+  namespace Jss {
+    export interface Theme {
+      red: string;
+    }
+  }
+}
+
+export {};
