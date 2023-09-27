d4ab4ff4da80f4f65ce9675a1c0b235917ec59f5
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-27 18:56:46 +0100

    Convert markdown to static html

--- packages/resolute/src/cli/constants.ts
+++ packages/resolute/src/cli/constants.ts
@@ -24,6 +24,7 @@ export const PUBLIC_FILES_GLOB = 'public/**/*';
 export const MATCHES_LOCAL = /^[./]/;
 export const MATCHES_NODE_MODULE = /.*\/node_modules\//;
 export const MATCHES_RESOLUTE = /@blinkorb\/resolute|resolute\/build/;
+export const MATCHES_MARKDOWN = /\.(md|markdown)$/;
 export const MATCHES_PAGE = /\.page\.js$/;
 export const MATCHES_CLIENT = /\.client\.js$/;
 export const MATCHES_LAYOUT = /\.layout\.js$/;
@@ -36,3 +37,4 @@ export const MATCHES_JS_EXTENSION = /\.[mc]?js$/;
 
 export const GLOB_JS_EXTENSION = '.{cjs,mjs,js}';
 export const GLOB_SRC_EXTENSION = '.{cjs,mjs,js,jsx,ts,tsx,mts}';
+export const GLOB_MARKDOWN_EXTENSION = '.{md,markdown}';
