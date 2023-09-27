d874d0a1a6a8adac809b1250c084b5fdabdd8e5f
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-27 17:55:40 +0100

    Update path utils to resolve .ts files and handle markdown paths

--- packages/resolute/src/cli/utils/paths.ts
+++ packages/resolute/src/cli/utils/paths.ts
@@ -1,27 +1,39 @@
+import fs from 'node:fs';
 import path from 'node:path';
 
 import { withLeadingAndTrailingSlash } from '../../utils/paths.js';
 import { SERVER_PATHNAME, SRC_PATHNAME } from '../constants.js';
 
-export const fromServerPathToRelativeTSX = (pathname: string) =>
-  path.resolve(
+export const fromServerPathToRelativeTSX = (pathname: string) => {
+  const resolved = path.resolve(
     SRC_PATHNAME,
     path.relative(SERVER_PATHNAME, pathname).replace(/\.js$/, '.tsx')
   );
 
-export const pathnameToRoute = (pathname: string) =>
+  if (fs.existsSync(resolved)) {
+    return resolved;
+  }
+
+  return resolved.replace(/\.tsx$/, '.ts');
+};
+
+export const pathnameToRoute = (pathname: string, root: string) =>
   path
-    .relative(SERVER_PATHNAME, pathname)
+    .relative(root, pathname)
     // Make absolute
     .replace(/^(\.\/)?/, '/')
     // Remove hidden routes
     .replace(/\/_[\w-]+\//, '/')
+    // Resolve index.md
+    .replace(/\/index\.(md|markdown)$/, '/')
+    // Remove markdown extension
+    .replace(/\.(md|markdown)$/, '/')
     // Resolve module.index.js
-    .replace(/\/index\.(page|client|static|server|layout)\.js/, '/')
+    .replace(/\/index\.(page|client|static|server|layout)\.js$/, '/')
     // Resolve index.html
     .replace(/\/index\.html$/, '/')
-    // Resolve directories
-    .replace(/\.(page|client|static|server|layout)\.js/, '/');
+    // Remove namespace and js extension
+    .replace(/\.(page|client|static|server|layout)\.js$/, '/');
 
 export const isPartialRouteMatch = (route: string, match: string) =>
   withLeadingAndTrailingSlash(route).startsWith(
