6615b698a81f3575b9189aecd28acd4d4e614100
Jake 'Sid' Smith <hello@jakesidsmith.com>
2023-09-27 19:17:36 +0100

    Add second styles to reused component

--- packages/example/src/components/counter-client.tsx
+++ packages/example/src/components/counter-client.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles((theme) => ({
+  button: {
+    border: `1px solid ${theme.red}`,
+  },
+}));
 
 const CounterClient = () => {
+  const styles = useStyles();
   const [count, setCount] = useState(0);
 
   return (
     <>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
+      <button className={styles.button} onClick={() => setCount(count + 1)}>
+        +
+      </button>
     </>
   );
 };
